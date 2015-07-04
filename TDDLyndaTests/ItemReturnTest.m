3816f68ac2a8bdbb004ee39c6cf48867a8d21111
ANDRES KWAN <andreskwan@gmail.com>
2015-07-04 13:45:49 -0500

    schemes were added to allow bot to work on server

--- TDDLyndaTests/ItemReturnTest.m
+++ TDDLyndaTests/ItemReturnTest.m
@@ -10,7 +10,7 @@
 #import "ItemReturn.h"
 
 @interface ItemReturnTest : XCTestCase
-
+@property (nonatomic, strong) ItemReturn * itemReturn;
 @end
 
 @implementation ItemReturnTest
@@ -18,19 +18,26 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.itemReturn = [[ItemReturn alloc] init];
 }
 
 - (void)tearDown {
+    self.itemReturn = nil;
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+
 }
 
 - (void)testReturnMethod {
     // This is an example of a functional test case.
-    ItemReturn * itemReturn = [[ItemReturn alloc]init];
-    id shouldBeString = [itemReturn returnString];
+    id shouldBeString = [self.itemReturn returnString];
     XCTAssertTrue([shouldBeString isKindOfClass:[NSString class]], @"%s don't retutn a string", __PRETTY_FUNCTION__);
 }
 
+- (void)testFail {
+    // This is an example of a functional test case.
+    id shouldBeString = [self.itemReturn returnString];
+    XCTAssertNotNil(shouldBeString, @"%s don't retutn a string", __PRETTY_FUNCTION__);
+}
 
 @end
