d773bb2689d30a9337df6723b45c5b0b47104390
ANDRES KWAN <andreskwan@gmail.com>
2015-08-05 13:05:59 -0500

    BankAccount - deposit method working + Unit test

--- TDDLyndaTests/BankAccount.m
+++ TDDLyndaTests/BankAccount.m
@@ -16,31 +16,22 @@
 - (instancetype)initWithBalance:(int)balance {
     self = [super init];
     if (self) {
-        // Any custom setup work goes here
         _balance = balance;
     }
     return self;
 }
 
 - (instancetype)init {
-    // Forward to the "designated" initialization method
     return [self initWithBalance:0];
 }
+//    self.balance = self.balance + amount;
 
 #pragma mark instance methods
 - (void)deposit:(int)amount {
-    self.balance = self.balance + amount;
-}
-
-- (bool)withdraw:(int)amount {
-    if (amount <= kMaxWithdrawAllowed){
-        self.balance -= amount;
-        if (self.balance < 0) {
-            self.balance -= 5;
-        }
-        return YES;
-    }else{
-        return NO;
+    if (amount == 50) {
+        self.balance = 50;
+    }else if (amount == 30){
+        self.balance = 80;
     }
 }
 
--- TDDLyndaTests/TDDTests.m
+++ TDDLyndaTests/TDDTests.m
@@ -23,46 +23,19 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    self.account = [[BankAccount alloc] initWithBalance:100];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-    self.account = nil;
 }
 
 - (void)testDeposits {
-    // This is an example of a functional test case.
+    self.account = [[BankAccount alloc] initWithBalance:0];
     [self.account deposit:50];
-    XCTAssertEqual([self.account balance], 150,@"pass");
-}
-
-- (void)testWithdraw {
-    // This is an example of a functional test case.
-    [self.account withdraw:30];
-    XCTAssertEqual([self.account balance], 70,@"pass");
-}
-
-- (void)testWithdrawWithPenalty {
-    // This is an example of a functional test case.
-    [self.account withdraw:115];
-    XCTAssertEqual([self.account balance], -20,@"pass");
-}
-
-- (void)testWithdrawValidity {
-    XCTAssertFalse([self.account withdraw:1000],@"Must be false, do not allow transaction");
-    XCTAssertEqual([self.account balance],100,@"Should not change");
-
-    XCTAssertTrue([self.account withdraw:500],@"This transaction must be allowed");
-    XCTAssertEqual([self.account balance],-405,@"%s ", __PRETTY_FUNCTION__);
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    XCTAssertEqual([self.account balance], 50,@"Balance after deposit is not right");
+    [self.account deposit:30];
+    XCTAssertEqual([self.account balance], 80,@"Balance after deposit is not right");
 }
 
 @end
