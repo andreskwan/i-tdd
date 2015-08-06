42667c5f0a82a8557beb449f8d95175a75801ae6
ANDRES KWAN <andreskwan@gmail.com>
2015-08-06 07:44:47 -0500

    adding pods for Specta and Expecta

---
+++ BDDBankAccountTests/BDDBankAccountTests.m
@@ -0,0 +1,40 @@
+//
+//  BDDBankAccountTests.m
+//  BDDBankAccountTests
+//
+//  Created by Andres Kwan on 8/6/15.
+//  Copyright (c) 2015 ___PEPE___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BDDBankAccountTests : XCTestCase
+
+@end
+
+@implementation BDDBankAccountTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'TDDLynda' do
+
+end
+
+target 'TDDLyndaTests' do
+
+end
+
+target 'BDDBankAccountTests' do
+	pod 'Specta'
+	pod 'Expecta'
+end
+
